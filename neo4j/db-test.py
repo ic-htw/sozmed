aba57f16028646656e3a2f0eab9bde96cae136fe
Ingo Classen <ingo.classen@htw-berlin.de>
2023-02-22 15:00:38 +0100

    wip

--- neo4j/db-test.py
+++ neo4j/db-test.py
@@ -8,7 +8,7 @@ pd.set_option("display.max_columns", None)
 
 with GraphDatabase.driver(c.neo4j_host, auth=(c.neo4j_userid, c.neo4j_password)) as driver:
     q = f"""
-    MATCH (x:Forum) return count(*);
+    MATCH (x:Message) return count(*);
     """
     with driver.session() as session:
         rs = session.run(q)
